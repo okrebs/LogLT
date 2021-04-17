bf750ca3ebd44f7de283c25e3672e4a5e5ad73cc
okrebs <oliver.b.krebs@gmail.com>
2021-04-17 02:35:50 +0200

    clean code of task2

--- R/task2-final.R
+++ R/task2-final.R
@@ -115,258 +115,136 @@
 
 
 
-calc_cf_log_t2=function(simple_data,shocks,parameters){
-
-
-
-  #######################################################
-  ###### step 2: Computation of necessary matrices ######
-  #######################################################
-
-
-
-  diaggamma=diag(c(gamma));  ###### diagonal matrix of gamma
-  IJ=diag(J)                 ###### Identity matrix
-
-  Igammadiag=IJ-diaggamma    ###### Identity-diaggama
-
-  pi_Igamma=crossprod(pi_I,Igammadiag)  ###### pi_I'(identity-diaggama)
-  pi_Fgamma=crossprod(pi_F,Igammadiag)  ###### pi_F'(identity-diaggama)
-  col=matrix(colSums(pi_I),ncol=2)      ###### vector w elements from pi_I pi11+pi21, pi12+pi22
-  colF=matrix(colSums(pi_F),ncol=2)     ###### vector w elements from pi_F pi11+pi21, pi12+pi22
-
-  #transpose of pi_I and pi_F
-  tpi_I=t(pi_I)
-  tpi_F=t(pi_F)
-
-
-
-  Rrecip=c(sapply(R, function(x) 1/x))  #getting the reciprocal of R vector (1/Ri)
-
-
-  RR=crossprod(t(Rrecip),R)
-
-  A=(crossprod(t(Rrecip),R)*(pi_I%*%Igammadiag))   #### matrix A def
-
-  ################## B & Bp caculations
-
-
-  B=epsilon*(crossprod(t(Rrecip),R))*((pi_I)%*%(Igammadiag)%*%diag(c(colSums(pi_I))))
-
-  Bp=epsilon*(crossprod(t(Rrecip),R))*(pi_F%*%diaggamma%*%diag(c(colSums(pi_F))))
-
-
-
-
-  #################  Matrix A&Ap calculation
-
-  Rrecip=c(sapply(R, function(x) 1/x))  #getting the reciprocal of R vector (1/Ri)
-
-
-  RR=crossprod(t(Rrecip),R)
-
-  A=(crossprod(t(Rrecip),R)*(pi_I%*%Igammadiag))   #### matrix A def
-
-  Ap=(crossprod(t(Rrecip),R)*(pi_F%*%diaggamma))
-
-  gR=R*gamma
-  Q= matrix(rep(gR,each=J), ncol=J, byrow=FALSE)
-
-  ######################################################################
-  ######step3: Creating Block Matrices: computing matrix M #############
-  ######################################################################
-
-  eA=epsilon*A
-  eAp=epsilon*Ap
-
-  M1=crossprod(pi_I,Igammadiag)
-  M2=crossprod(pi_I,diaggamma)
-  M3=(eA%*%M1)+(eAp%*%pi_Fgamma)-(epsilon*Igammadiag)
-  M4=A+Ap+(eA%*%tpi_I%*%diaggamma)+(eAp%*%tpi_F%*%diaggamma)-epsilon*diaggamma-Q
-
-  M=matrix(rbind(cbind(M1,M2),cbind(M3,M4)),ncol=4)
-
-
-
-
-
-  ##################### computing the spectral radius of M
-
-
-  spectral_R_M=max(abs(eigen(M)$value))
-
-
-  ############################################################################
-  ##################### step 4: creating the if else blocks.      ############
-  ##################### if spectral radius < 1: function proceeds ############
-  ##################### to compute dlogC                          ############
-  ##################### else it stops;                            ############
-  ############################################################################
-
-  if(spectral_R_M<1){
-
-
-
-
-    ############################################################################
-    #####################                            ###########################
-    ##################### Step 5: computing V matrix ###########################
-    #####################                            ###########################
-    ############################################################################
-
-    vv1=(-1/epsilon)*tpi_I
-    vv2=IJ-(A%*%tpi_I)-(Ap%*%tpi_F)
-    V1=rbind(vv1,vv2)
-
-
-
-    vvv1=matrix(diag((tpi_I)%*%tau_hat_I),nrow=2)
-    vvv2=matrix(eA%*%diag((tpi_I)%*%tau_hat_I)+(eAp%*%diag((tpi_F)%*%tau_hat_F))-(diag(B%*%t(tau_hat_I)))-diag(Bp%*%t(tau_hat_F)),nrow=2)
-
-    V2=rbind(vvv1,vvv2)
-
-
-    V=(V1%*%T_hat)+V2
-
-
-
-
-    #######################################################################
-    #######################################################################
-    ###################                                               #####
-    ################### step 6: Computing C_hat based on V(I-M)^(-1) #####
-    ###################                                               #####
-    #######################################################################
-    #######################################################################
-
-
-    z=solve(diag(J*J)-M)
-
-
-    pifg=-(pi_Fgamma)
-    pifI=IJ-(tpi_F%*%diaggamma)
-    pigamma=cbind(pifg,pifI)
-
-
-
-
-
-
-    S1=-diag(crossprod(pi_F,tau_hat_F))
-    S2=(1/epsilon)*tpi_F%*%(T_hat)
-
-
-
-
-
-
-    C_hat_inv=S1+S2+(pigamma%*%z%*%V)
-
-
-    #######################################################################
-    #######################################################################
-    ###################                                               #####
-    ################### step 6: Computing C_hat= based on I+M2+M3+... #####
-    ###################                                               #####
-    #######################################################################
-    #######################################################################
-
-
-
-
-
-    #######################################################################
-    #######################################################################
-    ###################*** step 6': Important Step: While loop   ##########
-    ###################*   to make sure                          ##########
-    ###################*   Convergence happens.                  ##########
-    ###################*   Converg2 is a big number=1            ##########
-    ###################*   in loop: while converg2>10^(-20)      ##########
-    ###################*   it continues to compute M^i           ##########
-    ###################*   and stack all in a list               ##########
-    ###################*   and add all of them together          ##########
-    ###################*   to get (I+M+M2+....)                  ##########
-    ###################*   while the difference of               ##########
-    ###################*   sumsquare(V(I-M)^(-1)-(I+M+M2+....))=converg2 ##
-    ###################*                                         ##########
-    #######################################################################
-    #######################################################################
-
-    converg2 = 1
-    i=0
-    m5 = list();
-    conv2msg=list();
-    nthmsg2=list();
-    nth2effect=list();
-
-    while(converg2>(10^(-20))) {
-      m5[i+1]=list(M %^% i);
-
-      a5=apply(simplify2array(m5), c(1,2), sum)
-      converg2=colSums(matrix(colSums(apply(z-(a5),c(1,2),'^',2))))
-      if (i == 0){
-        nth2effect[i+1]=list(S1+S2+(pigamma%*%matrix(unlist(m5[i+1]),ncol=J*J,nrow=J*J)%*%V))
+calc_cf_log_t2=function(J, R, pi_I, pi_F, gamma, T_hat, tau_hat_I, tau_hat_F,
+                        epsilon, ktol = 1e-10, kmax_iter = 500){
+
+  # step 1: Computation of necessary matrices ##################################
+  diaggamma <- diag(gamma)
+  IJ <- diag(J)
+  Igammadiag <- IJ - diaggamma
+
+  pi_Igamma <- crossprod(pi_I, Igammadiag)
+  pi_Fgamma <- crossprod(pi_F, Igammadiag)
+  tpi_I <- t(pi_I)
+  tpi_F <- t(pi_F)
+
+  Rrecip <- 1/R
+  RR <- crossprod(Rrecip, R)
+
+  A <- RR * (pi_I %*% Igammadiag)
+  Ap <-  RR * (pi_F %*% diaggamma)
+  B <- epsilon * A
+  Bp <- epsilon * Ap
+
+  Q <- matrix(rep(R * gamma, each = J), ncol = J)
+
+  # step3: Creating Block Matrices: computing matrix M #########################
+  M1 <- crossprod(pi_I, Igammadiag)
+  M2 <- crossprod(pi_I, diaggamma)
+  M3 <- B %*% M1 + Bp %*% pi_Fgamma - epsilon * Igammadiag
+  M4 <- A + Ap + B %*% tpi_I %*% diaggamma + Bp %*% tpi_F %*% diaggamma -
+    epsilon * diaggamma - Q
+
+  M <- matrix(rbind(cbind(M1, M2), cbind(M3, M4)), ncol = 4)
+
+  # step 4: test spectral radius ###############################################
+  spectral_R_M  <- max(abs(eigen(M)$value))
+
+  if(spectral_R_M >= 1) {
+    stop("The decomposition does not work since the spectral radius of M is ",
+         "greater than 1")
+  } else {
+    # step 5: computing V matrix ###############################################
+    vv1 <- -1 / epsilon * tpi_I
+    vv2 <- IJ- A %*% tpi_I - Ap%*%tpi_F
+    V1 <- rbind(vv1,vv2)
+
+    vvv1 <- matrix(diag(tpi_I %*% tau_hat_I), nrow = 2)
+    vvv2 <- matrix(B %*% diag(tpi_I %*% tau_hat_I) +
+                     Bp %*% diag(tpi_F %*% tau_hat_F) -
+                     diag(B %*% t(tau_hat_I)) -
+                     diag(Bp %*% t(tau_hat_F)),
+                   nrow=2)
+    V2 <- rbind(vvv1, vvv2)
+    V <- V1 %*% T_hat + V2
+
+    # step 6: Computing C_hat based on V(I-M)^(-1) #############################
+    z <- solve(diag(J * J) - M)
+    pifg <- -pi_Fgamma
+    pifI <- IJ - tpi_F %*% diaggamma
+    pigamma <- cbind(pifg, pifI)
+
+    S1 <- -diag(crossprod(pi_F, tau_hat_F))
+    S2 <- 1 / epsilon * tpi_F %*% T_hat
+    C_hat_inv <- S1 + S2 + pigamma %*% z %*% V
+
+    # step 7: Computing C_hat = based on I + M2 + M3 +... ######################
+    # loop to make sure convergence happens. Converg2 is a big number=1
+    # in loop: while converg2>10^(-20) it continues to compute M^i and stack all
+    # in a list and add all of them together to get (I+M+M2+....) while the
+    # difference of sumsquare(V(I-M)^(-1)-(I+M+M2+....))=converg2
+    converg2 <- 1
+    i <- 0
+    m5 <- list()
+    conv2msg <- list()
+    nthmsg2 <- list()
+    nth2effect <- list()
+
+    while(converg2 > ktol & i < kmax_iter) {
+
+      m5[i + 1] <- list(M %^% i)
+
+      a5 <- apply(simplify2array(m5), c(1,2), sum)
+      converg2 <- colSums(matrix(colSums(apply(z - a5, c(1,2), '^', 2))))
+
+      tmp <- pigamma %*%
+        matrix(unlist(m5[i + 1]), ncol = J * J, nrow = J*J) %*% V
+      if (i == 0) {
+        tmp <- S1 + S2 + tmp
       }
-      else {
-        nth2effect[i+1]=list((pigamma%*%matrix(unlist(m5[i+1]),ncol=J*J,nrow=J*J)%*%V))
-      }
-      nthmsg2[i+1]=list(sprintf("The effect of degree %s is %s",i,nth2effect[i+1]))
+      nth2effect[i + 1] <- list(tmp)
 
-      i=i+1;
+      nthmsg2[i + 1] <- list(
+        sprintf("The effect of degree %s is %s", i, nth2effect[i + 1])
+      )
+
+      i <- i + 1
     }
 
-
-
     ################ c_hat based on summation is :
-    C_hat_sum2=S1+S2+pigamma%*%a5%*%V
-    converg2;
+    C_hat_sum2 <- S1 + S2 + pigamma %*% a5 %*% V
 
+    #  step 8: final messages to be returned and printed #######################
 
+    finalmsg2 <-
+      sprintf(paste0("The difference of summation and V(I-M)^-1 using the ",
+                     "criteria of convergence is %s and the convergence ",
+                     "happens at %s repetition"), converg2, length(m5))
+    radius <-
+      sprintf(paste0("The decomposition works since the spectral radius is ",
+                     ",%s, which is smaller than 1"), spectral_R_M)
 
-    ##########################################################################
-    #########################  Step 7:                                   #####
-    #########################  final messages to be returned and printed #####
-    #########################                                            #####
-    ##########################################################################
+    C_hat_inv_msg <- sprintf("dlogC based on V(I_M)^-1 is %s", list(C_hat_inv))
 
-    finalmsg2=sprintf("The difference of summation and V(I-M)^-1 using the criteria of convergence is %s and the convergence happens at %s repetition",converg2,length(m5))
-    radius=sprintf("The decomposition works since the spectral radius is ,%s, which is smaller than 1",spectral_R_M)
+    zmsg <- sprintf("V(I_M)^-1 is")
+    criteriamsg <-
+      print(paste0("The criteria of convergence is sigma(i,j)(V(I-M)^(-1) - ",
+                   "(I+M+M2+M3+...)(i,j) which is the sum of the square of ",
+                   "all elements of the difference matrix"))
 
-    C_hat_inv_msg=sprintf("dlogC based on V(I_M)^-1 is %s",list(C_hat_inv))
-
-    zmsg=sprintf("V(I_M)^-1 is")
-    criteriamsg=print("The criteria of convergence is sigma(i,j)(V(I-M)^(-1)-(I+M+M2+M3+...)(i,j)  which is the sum of the square of all elements of the difference matrix")
-
-    a5msg=sprintf("The sum of all degrees is")
-    C_hat_sum_msg2=sprintf("dlogC based on the sum of all degree effects using the criteria of convergence is %s",list(C_hat_sum2))
+    a5msg <- sprintf("The sum of all degrees is")
+    C_hat_sum_msg2 <-
+      sprintf(paste0("dlogC based on the sum of all degree effects using the ",
+                     "criteria of convergence is %s"), list(C_hat_sum2))
   }
-
-
-
-
-  else{ print("The decomposition does not work since the spectral radius of M is greater than 1")}
-
-
-
-
-
-  return(list(radius,zmsg,matrix(z,ncol=J*J,nrow=J*J),a5msg,matrix(a5,ncol=J*J,nrow=J*J),C_hat_inv_msg,C_hat_sum_msg2,criteriamsg,finalmsg2,nthmsg2))
-
+  list(radius,
+       zmsg,
+       matrix(z, ncol = J*J, nrow = J*J),
+       a5msg,
+       matrix(a5, ncol = J*J, nrow = J*J),
+       C_hat_inv_msg,
+       C_hat_sum_msg2,
+       criteriamsg,
+       finalmsg2,
+       nthmsg2)
 }
-
-
-
-
-########################################## example in task 2 printout :
-simple_data=list(c(1,1.5),c(0.6,0.4,0.2,0.8,0.5,0.5,0.4,0.6),c(0.5,0.5))
-epsilon=3
-
-
-
-shocks <-
-  list(c(0.1,0),c(0,0,0,0,0,0,0,0))
-
-
-calc_cf_log_t2(simple_data,shocks,parameters)
-
-calc_cf_log_m_2(simple_data,shocks,epsilon)
